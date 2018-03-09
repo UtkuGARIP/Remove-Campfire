de0d3a4c067694a2f9f318337bd9c29d3adc7e7f
Utku Garip <utku.garip@gmail.com>
2018-03-09 17:13:44 +0300

    UtkuGARIP tarafından Türkçe Dil Eklenmiştir
    UtkuGARIP tarafından Türkçe Dil Eklenmiştir

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Remove-Campfire")]
+[assembly: AssemblyTitle("Kamp-Ateşi-Kaldırıcı")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Remove-Campfire")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Kamp-Ateşi-Kaldırıcı")]
+[assembly: AssemblyCopyright("Telif Hakkı ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
